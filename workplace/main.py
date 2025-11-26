271d73c635307ed47009aa203fbeef2605985eab
Forest <43562257+CrossStyle@users.noreply.github.com>
2025-11-26 16:02:29 +0800

    Update main.py

--- workplace/main.py
+++ workplace/main.py
@@ -12,7 +12,7 @@ from robofab.mqtt import publish_robot_trajs, publish_clear, publish_world, publ
 from robofab.place import poses_to_frames
 from curobo.geom.types import WorldConfig, Mesh, Cylinder, Cuboid
 from curobo.geom.types import Pose
-
+import pickle
 from workplace.gen_scaffold_plan import get_design
 from workplace.util import (load_kuka_kin_model,
                             tube_info,
@@ -70,15 +70,9 @@ if __name__ == "__main__":
     init_frames = np.vstack(init_frames)
     publish_frames(init_frames)
 
-    tube_id = 16
-    rotated_frames, tube_poses = generate_rotated_frames(tube_frames[tube_id], num_steps=72)
-    publish_frames(rotated_frames)
-
-    # fs = []
-    # for tube_p in poses:
-    #     fs.append(poses_to_frames(tube_p))
-    # fs = np.vstack(fs)
-    # publish_frames(fs)
+    # tube_id = 16
+    # rotated_frames, tube_poses = generate_rotated_frames(tube_frames[tube_id], num_steps=72)
+    # publish_frames(rotated_frames)
 
     # place_result = plan_ik(tube_poses, place_world, 0.15)
     # traj, attached_object_for_rendering = compute_pick_place(tube_id, tube_poses[tube_id], tube_mesh[tube_id], place_world, robot_kin_model)
@@ -87,6 +81,7 @@ if __name__ == "__main__":
 
     # traj, attached_object_for_rendering = compute_pick_place(tube_id)
 
+    # motion planning
     # final_trajs = np.empty((0, len(robot_kin_model.joint_names)))
     # save_data = {
     #     'seq': [],
@@ -121,14 +116,48 @@ if __name__ == "__main__":
     #     except RuntimeError as e:
     #         print(f"Error processing tube_id {tube_id}: {e}")
     #
-    # import pickle
     # with open("save_data.pkl", "wb") as f:
     #     pickle.dump(save_data, f)
 
-    # tube_cylinder, tube_mesh, tube_pose = tube_info(copy.deepcopy(scaffold_structure), valid_tube_index)
-    # for tube_p in tube_pose:
-    #     frame = poses_to_frames(tube_p)
 
-    # tube_id = 15  # 21
-    # frames = poses_to_frames(tube_pose[tube_id])
-    # publish_frames(init_frames)
+    with open("save_data.pkl", "rb") as f:
+        save_data = pickle.load(f)
+
+
+    visual_world = WorldConfig(
+        cylinder=[
+            Cylinder(
+                name="col1",
+                radius=0.4,
+                height=1.2 * 2,
+                pose=[3.2, -0.8, 1.2, np.sin(1.57 / 2), 0, 0, np.cos(1.57 / 2)],
+                color=[0.8, 0.6, 0.4, 1.0],
+            ),
+            Cylinder(
+                name="col2",
+                radius=0.4,
+                height=1.2 * 2,
+                pose=[3.2, 0.8, 1.2, np.sin(1.57 / 2), 0, 0, np.cos(1.57 / 2)],
+                color=[0.8, 0.6, 0.4, 1.0],
+            ),
+            # tube_cylinder[2]
+        ],
+        cuboid=[Cuboid(
+            name="wall",
+            dims=[2.0 * 2, 0.025 * 2, 2.4],
+            pose=[3.2, 0, 1.2, np.sin(1.57 / 2), 0, 0, np.cos(1.57 / 2)],
+            color=[0.8, 0.6, 0.4, 1.0],
+        )]
+    )
+
+    for tube_id in save_data['seq']:
+        traj = save_data['traj'][tube_id]['tube_traj']
+        attached_object_for_rendering = save_data['traj'][tube_id]['attached_object_for_rendering']
+        visual_world = WorldConfig(
+        cylinder=[*visual_world.cylinder, tube_cylinder[tube_id]],
+        cuboid=[*visual_world.cuboid],
+        )
+        publish_robot_trajs(traj, 0.15)
+        publish_grasp_object(attached_object_for_rendering)
+        publish_world(visual_world, merged=True)
+
